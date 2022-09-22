353c59098ed0be2317baecabac80828191c43452
asllanmakaj7 <107688754+asllanmakaj7@users.noreply.github.com>
2022-09-22 16:06:16 +0200

    tsconfig, jest, global

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+/*
+	Unless explicitly acquired and licensed from Licensor under another
+	license, the contents of this file are subject to the Reciprocal Public
+	License ("RPL") Version 1.5, or subsequent versions as allowed by the RPL,
+	and You may not copy or use this file in either source code or executable
+	form, except in compliance with the terms and conditions of the RPL.
+
+	All software distributed under the RPL is provided strictly on an "AS
+	IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND
+	LICENSOR HEREBY DISCLAIMS ALL SUCH WARRANTIES, INCLUDING WITHOUT
+	LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+	PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific
+	language governing rights and limitations under the RPL. 
+*/
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.export ={
+	moduleNameMapper:{
+		'@shko-online/componentframework-mock/(.*)': '<rootDir>/ComponentFramework-Mock/src/$1',
+	},
+	preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    transform: {
+      // transform files with ts-jest
+      "^.+\\.(jsx?|tsx?)$": "ts-jest",
+      "\\.resx" : "<rootDir>/raw-Loader.js"
+    },
+    
+    coveragePathIgnorePatterns : [
+      "/node_modules/",
+      "/ComponentFramework-Mock/"
+    ],
+    coverageReporters: ['cobertura', "text","html"]
+}
