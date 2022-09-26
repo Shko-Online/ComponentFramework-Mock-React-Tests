0d6fac37f7fcd703b7e83d0d50409363d20ffa99
artjola zalla <78443274+tjola@users.noreply.github.com>
2022-09-26 17:35:04 +0200

    context story items update

--- stories/ContextMenu.stories.tsx
+++ stories/ContextMenu.stories.tsx
@@ -22,7 +22,6 @@ import {ContextMenu} from "@powercat/context-menu/ContextMenu"
 import { StringPropertyMock } from "@shko-online/componentframework-mock/ComponentFramework-Mock/PropertyTypes/StringProperty.mock";
 import { TwoOptionsPropertyMock } from "@shko-online/componentframework-mock/ComponentFramework-Mock/PropertyTypes/TwoOptionsProperty.mock";
 import { EnumPropertyMock } from "@shko-online/componentframework-mock/ComponentFramework-Mock/PropertyTypes/EnumProperty.mock";
-import { DateTimePropertyMock } from "@shko-online/componentframework-mock/ComponentFramework-Mock/PropertyTypes/DateTimeProperty.mock";
 import { WholeNumberPropertyMock } from "@shko-online/componentframework-mock/ComponentFramework-Mock/PropertyTypes/WholeNumberProperty.mock";
 import { DataSetMock } from "@shko-online/componentframework-mock/ComponentFramework-Mock/PropertyTypes/DataSet.mock";
 import { EntityRecord } from "@shko-online/componentframework-mock/ComponentFramework-Mock/PropertyTypes/DataSetApi/EntityRecord.mock";
@@ -37,6 +36,8 @@ export default {
   // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
   argTypes: {},
 } as Meta;
+
+type AlignmentTypes = "0" | "1" | "2"; 
 const Template = (args) => {
     const mockGenerator: ComponentFrameworkMockGeneratorReact<IInputs, IOutputs> =
       new ComponentFrameworkMockGeneratorReact(ContextMenu, {
@@ -52,7 +53,7 @@ const Template = (args) => {
       BorderColor:  StringPropertyMock,
       HoverBorderColor:  StringPropertyMock,
       BorderRadius:  WholeNumberPropertyMock,
-      TextAlignment: EnumPropertyMock<"0" | "1" | "2">,
+      TextAlignment: EnumPropertyMock<AlignmentTypes>,
       AccessibilityLabel:  StringPropertyMock,
       Theme:  StringPropertyMock,
       InputEvent:  StringPropertyMock,
@@ -63,19 +64,27 @@ const Template = (args) => {
   items.initRecords(
     [1].map((item) => {
       const row = new EntityRecord("test", '1', 'Item 1');
-      row.columns['Key']= 'item1';
-      row.columns['DisplayName']= 'Item 1';
-      row.columns["IconName"]= 'Open';
-      row.columns["IconColor"]= 'blue';
-      row.columns["Enabled"]= true;
-      row.columns["IconOnly"]= false;
+      row.columns['ItemKey']= 'item1';
+      row.columns['ItemDisplayName']= 'Item 1';
+      row.columns["ItemIconName"]= 'Open';
+      row.columns["ItemIconColor"]= 'blue';
+      row.columns["ItemEnabled"]= true;
+      row.columns["ItemIconOnly"]= false;
       return row;
     })
-  );
 
+  );
 
 mockGenerator.ExecuteInit();
 const component = mockGenerator.ExecuteUpdateView();
 return component;
 };
 export const Primary = Template.bind({});
+Primary.args = {
+       Key: 'item1',
+       DisplayName: 'Item 1',
+       IconName: 'Open',
+       IconColor: 'blue',
+       Enabled: true,
+       IconOnly: false,
+  };
