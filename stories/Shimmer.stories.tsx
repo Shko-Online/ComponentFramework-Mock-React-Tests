e1ffdcd3cf833118f8e2b404fa590251a5f3313b
artjola zalla <78443274+tjola@users.noreply.github.com>
2022-09-30 10:11:45 +0200

    added items to shimmer

--- stories/Shimmer.stories.tsx
+++ stories/Shimmer.stories.tsx
@@ -24,10 +24,12 @@ import { StringPropertyMock } from '@shko-online/componentframework-mock/Compone
 import { WholeNumberPropertyMock } from '@shko-online/componentframework-mock/ComponentFramework-Mock/PropertyTypes/WholeNumberProperty.mock';
 import { EnumPropertyMock } from '@shko-online/componentframework-mock/ComponentFramework-Mock/PropertyTypes/EnumProperty.mock';
 import { DataSetMock } from '@shko-online/componentframework-mock/ComponentFramework-Mock/PropertyTypes/DataSet.mock';
+import { ItemColumns } from "@powercat/shimmer/Shimmer/ManifestConstants";
+import { EntityRecord } from "@shko-online/componentframework-mock/ComponentFramework-Mock/PropertyTypes/DataSetApi/EntityRecord.mock";
 
 
 export default {
-    title: 'PCF Components/Nav',
+    title: 'PCF Components/Shimmer',
     parameters: {
         // More on Story layout: https://storybook.js.org/docs/html/configure/story-layout
         layout: 'fullscreen',
@@ -47,10 +49,79 @@ const Template = (args) => {
             SpacebetweenShimmer:EnumPropertyMock<SpaceBetweenshimmer>,
             items:DataSetMock,
         });
-       
+
+        const Theme = mockGenerator.context.parameters.Theme as StringPropertyMock;
+        Theme.setValue(args.theme);
+        const AccessibilityLabel = mockGenerator.context.parameters.AccessibilityLabel as StringPropertyMock;
+        AccessibilityLabel.setValue(args.AccessibilityLabel);
+        const RowCount = mockGenerator.context.parameters.RowCount as WholeNumberPropertyMock;
+        RowCount.setValue(args.RowCount);
+        const SpacebetweenShimmer = mockGenerator.context.parameters.SpacebetweenShimmer as EnumPropertyMock<SpaceBetweenshimmer>;
+        SpacebetweenShimmer.setValue(args.SpaceBetweenshimmer);
+        const items = mockGenerator.context.parameters.items as DataSetMock;
+
+        items.initRecords(
+            (args.items || []).map((item) => {
+                const row = new EntityRecord('',item.id, item.id);
+    row.columns[ItemColumns.Height]=item[ItemColumns.Height];
+    row.columns[ItemColumns.Width]=item[ItemColumns.Width];
+    row.columns[ItemColumns.Type]=item[ItemColumns.Type];
+    row.columns[ItemColumns.Key]=item[ItemColumns.Key];
+    row.columns[ItemColumns.RowKey]=item[ItemColumns.RowKey];
+    
+        return row;
+    }),
+);
     mockGenerator.ExecuteInit();
     const component = mockGenerator.ExecuteUpdateView();
     return component;
 };
 
 export const Primary = Template.bind({});
+Primary.args = {
+    theme: JSON.stringify({
+        palette: {
+            themePrimary: '#test-primary',
+        },
+    }),
+    AccessibilityLabel: "Shimmer Component",
+    RowCount: 3,
+    SpaceBetweenshimmer: "10px",
+    items: [
+        {
+            id: '1',                
+            [ItemColumns.Height]: 25,
+            [ItemColumns.Width]: '3.8',
+            [ItemColumns.RowKey]: '1',
+            [ItemColumns.Type]: 'circle',
+        },
+        {
+            id: '2',                
+            [ItemColumns.Height]: 10,
+            [ItemColumns.Width]: '5',
+            [ItemColumns.RowKey]: '1',
+            [ItemColumns.Type]: 'gap',
+        },
+        {
+            id: '3',                
+            [ItemColumns.Height]: 20,
+            [ItemColumns.Width]: '100',
+            [ItemColumns.RowKey]: '1',
+            [ItemColumns.Type]: 'line',
+        },        
+        {
+            id: '4',                
+            [ItemColumns.Height]: 10,
+            [ItemColumns.Width]: '10',
+            [ItemColumns.RowKey]: '2',
+            [ItemColumns.Type]: 'gap',
+        },
+        {
+            id: '5',                
+            [ItemColumns.Height]: 20,
+            [ItemColumns.Width]: '100',
+            [ItemColumns.RowKey]: '2',
+            [ItemColumns.Type]: 'line',
+        }
+    ]
+}
